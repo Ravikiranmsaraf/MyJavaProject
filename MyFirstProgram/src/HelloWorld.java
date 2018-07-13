3228a33863dc5a2dd8ef4cc5b395cb2ec8948eb5
Ravikiranmsaraf <37213308+Ravikiranmsaraf@users.noreply.github.com>
2018-07-13 11:59:10 +0530

    First commit

---
+++ MyFirstProgram/src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		firstMethod();
+		}
+	
+	public static void firstMethod() {
+		System.out.println("From My fristMethod");
+		secondMethod();
+	}
+	public static void secondMethod() {
+		System.out.println("From My secondMethod");
+	}
+}
